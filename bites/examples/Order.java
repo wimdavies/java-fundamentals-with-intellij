ff54878aa35a0806a5a23dfc03ef352ee5cfb4b8
wimdavies <william.johnston.davies@gmail.com>
2024-05-28 16:22:04 +0100

    Add Order#friendlyTotal

--- bites/examples/Order.java
+++ bites/examples/Order.java
@@ -15,10 +15,14 @@ public class Order {
         this.total -= amount;
     }
 
+    public String friendlyTotal() {
+        return String.format("Thanks! The total value of your order is £%d.", getTotal());
+    }
+
     public static void main(String[] args) {
         Order order = new Order();
         order.addAmount(10);
         order.addDiscount(5);
-        System.out.println(order.getTotal());
+        System.out.println(order.friendlyTotal());
     }
 }
