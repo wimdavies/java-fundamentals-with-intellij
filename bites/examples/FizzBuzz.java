014f8146a762e15803371c929199f07755125796
wimdavies <william.johnston.davies@gmail.com>
2024-05-28 16:42:11 +0100

    Add FizzBuzz

---
+++ bites/examples/FizzBuzz.java
@@ -0,0 +1,21 @@
+package bites.examples;
+
+public class FizzBuzz {
+    public static String play(Integer number) {
+        if (number % 3 == 0 & number % 5 == 0) {
+            return "FizzBuzz";
+        } else if (number % 3 == 0) {
+            return "Fizz";
+        } else if (number % 5 == 0) {
+            return "Buzz";
+        } else {
+            return number.toString();
+        }
+    }
+
+    public static void main(String[] args) {
+        for (int i = 0; i <= 15; i++) {
+            System.out.println(FizzBuzz.play(i));
+        }
+    }
+}
