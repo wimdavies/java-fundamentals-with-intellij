8db2cecb145d521ff6d60d58ad79e23b1a61174d
wimdavies <william.johnston.davies@gmail.com>
2024-05-28 13:14:14 +0100

    Turn Announcement methods into instance methods

--- bites/examples/Announcement.java
+++ bites/examples/Announcement.java
@@ -2,30 +2,32 @@ package bites.examples;
 
 public class Announcement {
     public static void main(String[] args) {
-        System.out.println(Announcement.celebration());
-        System.out.println(Announcement.changeOfPlan());
-        System.out.println(Announcement.apology());
-        System.out.println(Announcement.politeNotice());
-        System.out.println(Announcement.helpNeeded());
+        Announcement announcement = new Announcement();
+
+        System.out.println(announcement.celebration());
+        System.out.println(announcement.changeOfPlan());
+        System.out.println(announcement.apology());
+        System.out.println(announcement.politeNotice());
+        System.out.println(announcement.helpNeeded());
     }
 
-    public static String celebration() {
+    public String celebration() {
         return "I'm learning this bloody language!";
     }
 
-    public static String changeOfPlan() {
+    public String changeOfPlan() {
         return "On second thoughts, maybe I'll give up.";
     }
 
-    public static String apology() {
+    public String apology() {
         return "Sorry, I hate this new IDE.";
     }
 
-    public static String politeNotice() {
+    public String politeNotice() {
         return "Nonetheless, I will still try.";
     }
 
-    public static String helpNeeded() {
+    public String helpNeeded() {
         return "But I need help!";
     }
 }
