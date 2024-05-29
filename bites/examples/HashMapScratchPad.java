f4ecffdae7d06e5ec7a8529e6411cd8155c1604f
wimdavies <william.johnston.davies@gmail.com>
2024-05-29 11:16:42 +0100

    Add HashMap ex.2

--- bites/examples/HashMapScratchPad.java
+++ bites/examples/HashMapScratchPad.java
@@ -1,17 +1,15 @@
 package bites.examples;
 
+import java.util.Arrays;
 import java.util.HashMap;
 
 public class HashMapScratchPad {
     public static void main(String[] args) {
-        HashMap<String, Integer> scores = new HashMap<>();
-        scores.put("The Blues", 0);
-        System.out.println(scores.get("The Blues"));
-        scores.put("The Reds", 0);
-        System.out.println(scores.get("The Reds"));
-        scores.put("The Blues", 5);
-        System.out.println(scores.get("The Blues"));
-        scores.put("The Reds", 3);
-        System.out.println(scores.get("The Reds"));
+        HashMap<String, Integer[]> scores = new HashMap<>();
+        scores.put("The Blues", new Integer[] {1, 2, 3});
+        scores.put("The Reds", new Integer[] {2, 3, 4});
+        System.out.println(scores.get("The Blues")[0]);
+        System.out.println(Arrays.toString(scores.get("The Blues")));
+        System.out.println(Arrays.toString(scores.get("The Reds")));
     }
 }
